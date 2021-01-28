1181945285c387c381302e6b700e75c0daac3429
Lizbeth <lizbaez.140799@gmail.com>
2021-01-28 11:23:21 -0700

    Plataforma2

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -6,24 +6,46 @@ public class Character2D : MonoBehaviour
 {
     Animator anim;
     SpriteRenderer spr;
-    [SerializeField, Range(0.1f, 20f)]
-    float moveSpeed = 2f;
+    [SerializeField, Range(0.1f, 10f)]
+    float moveSpeed = 1f;
+    [SerializeField, Range(0.1f, 15f)]
+    float jumpForce;
+    Rigidbody2D rb2D;
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 15f)]
+    float rayDistance = 5f;
+    [SerializeField]
+    LayerMask groundLayer;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
-        spr.flipX = Flip;
+        if(jumpButton && IsGrounding)
+        {
+            anim.SetTrigger("jump");
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }      
     }
 
     void LateUpdate()
     {
-        anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+        spr.flipX = flipSprite;
+       anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(rb2D.velocity.y);
+        anim.SetFloat("velocityY", rb2D.velocity.y);
     }
 
     Vector2 Axis
@@ -31,8 +53,16 @@ public class Character2D : MonoBehaviour
         get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
-    bool Flip
+    bool flipSprite
     {
         get => Axis.x > 0 ? false : Axis.x < 0f ? true : spr.flipX;
+    }
+    bool jumpButton => Input.GetButtonDown("Jump");
+    bool IsGrounding => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
     }
 }
